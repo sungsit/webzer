79d4996ac1f59910fd45670c978e9a0e7fdc6128
Sungsit Sawaiwan <sungsit.sawaiwan@gmail.com>
2013-03-26 21:19:17 +0700

    Fix compass config

--- config.rb
+++ config.rb
@@ -2,10 +2,10 @@
 
 # Set this to the root of your project when deployed:
 http_path = "/"
-css_dir = "styles"
+css_dir = "assets/styles"
 sass_dir = "_sass"
-images_dir = "images"
-javascripts_dir = "scripts"
+images_dir = "assets/images"
+javascripts_dir = "assets/scripts"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
